a0d65defa714ab003141257d356c4c79414fa23c
YP-Mac-02 <yp-mac-02@YP-Mac-02s-Mac-mini.local>
2026-06-16 16:26:55 +0800

    Update release repository owner

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	AppName     = "AgentPal"
 	AppVersion  = "0.1.2"
-	RepoOwner   = "cjb1234567"
+	RepoOwner   = "yprenewables"
 	RepoName    = "AgentPal"
 	DefaultPort = 28888
 	StateDir    = ".agentpal"
